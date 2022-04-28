a8784a003b84f5e3d1ed2d3874873f57a0b16a5c
guilherme.salmeida16 <guilherme.salmeida16@SBC0601503W10-1>
2022-04-28 11:57:43 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoDeTrocoMoeda/View/telaprincipal.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace CalculoDeTrocoMoeda.View
+{
+    partial class telaprincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValorCompra = new System.Windows.Forms.Label();
+            this.lblValorDinheiro = new System.Windows.Forms.Label();
+            this.lblValorTroco = new System.Windows.Forms.Label();
+            this.lblMoeda025 = new System.Windows.Forms.Label();
+            this.lblMoeda100 = new System.Windows.Forms.Label();
+            this.lblMoeda010 = new System.Windows.Forms.Label();
+            this.lblMoeda01 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblMoeda050 = new System.Windows.Forms.Label();
+            this.lblMoeda05 = new System.Windows.Forms.Label();
+            this.lblQtdMoeda01 = new System.Windows.Forms.Label();
+            this.lblQtdMoeda05 = new System.Windows.Forms.Label();
+            this.lblQtdMoeda100 = new System.Windows.Forms.Label();
+            this.lblQtdMoeda050 = new System.Windows.Forms.Label();
+            this.lblQtdMoeda010 = new System.Windows.Forms.Label();
+            this.lblQtdMoeda025 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.tbxValorCompra = new System.Windows.Forms.TextBox();
+            this.tbxValorDinheiro = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblValorCompra
+            // 
+            this.lblValorCompra.AutoSize = true;
+            this.lblValorCompra.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblValorCompra.Location = new System.Drawing.Point(57, 55);
+            this.lblValorCompra.Name = "lblValorCompra";
+            this.lblValorCompra.Size = new System.Drawing.Size(111, 15);
+            this.lblValorCompra.TabIndex = 0;
+            this.lblValorCompra.Text = "Valor da Compra:";
+            // 
+            // lblValorDinheiro
+            // 
+            this.lblValorDinheiro.AutoSize = true;
+            this.lblValorDinheiro.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblValorDinheiro.Location = new System.Drawing.Point(16, 92);
+            this.lblValorDinheiro.Name = "lblValorDinheiro";
+            this.lblValorDinheiro.Size = new System.Drawing.Size(152, 15);
+            this.lblValorDinheiro.TabIndex = 1;
+            this.lblValorDinheiro.Text = "Valor pago em dinheiro:";
+            // 
+            // lblValorTroco
+            // 
+            this.lblValorTroco.AutoSize = true;
+            this.lblValorTroco.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblValorTroco.Location = new System.Drawing.Point(83, 130);
+            this.lblValorTroco.Name = "lblValorTroco";
+            this.lblValorTroco.Size = new System.Drawing.Size(100, 15);
+            this.lblValorTroco.TabIndex = 2;
+            this.lblValorTroco.Text = "Valor do Troco:";
+            // 
+            // lblMoeda025
+            // 
+            this.lblMoeda025.AutoSize = true;
+            this.lblMoeda025.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMoeda025.Location = new System.Drawing.Point(621, 234);
+            this.lblMoeda025.Name = "lblMoeda025";
+            this.lblMoeda025.Size = new System.Drawing.Size(119, 15);
+            this.lblMoeda025.TabIndex = 3;
+            this.lblMoeda025.Text = "Moedas de R$ 0,25";
+            // 
+            // lblMoeda100
+            // 
+            this.lblMoeda100.AutoSize = true;
+            this.lblMoeda100.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMoeda100.Location = new System.Drawing.Point(621, 149);
+            this.lblMoeda100.Name = "lblMoeda100";
+            this.lblMoeda100.Size = new System.Drawing.Size(118, 15);
+            this.lblMoeda100.TabIndex = 4;
+            this.lblMoeda100.Text = "Moedas de R$ 1,00";
+            // 
+            // lblMoeda010
+            // 
+            this.lblMoeda010.AutoSize = true;
+            this.lblMoeda010.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMoeda010.Location = new System.Drawing.Point(621, 275);
+            this.lblMoeda010.Name = "lblMoeda010";
+            this.lblMoeda010.Size = new System.Drawing.Size(118, 15);
+            this.lblMoeda010.TabIndex = 5;
+            this.lblMoeda010.Text = "Moedas de R$ 0,10";
+            // 
+            // lblMoeda01
+            // 
+            this.lblMoeda01.AutoSize = true;
+            this.lblMoeda01.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMoeda01.Location = new System.Drawing.Point(621, 356);
+            this.lblMoeda01.Name = "lblMoeda01";
+            this.lblMoeda01.Size = new System.Drawing.Size(118, 15);
+            this.lblMoeda01.TabIndex = 6;
+            this.lblMoeda01.Text = "Moedas de R$ 0,01";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(188, 130);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(14, 15);
+            this.lblResultado.TabIndex = 7;
+            this.lblResultado.Text = "0";
+            // 
+            // lblMoeda050
+            // 
+            this.lblMoeda050.AutoSize = true;
+            this.lblMoeda050.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMoeda050.Location = new System.Drawing.Point(621, 190);
+            this.lblMoeda050.Name = "lblMoeda050";
+            this.lblMoeda050.Size = new System.Drawing.Size(119, 15);
+            this.lblMoeda050.TabIndex = 8;
+            this.lblMoeda050.Text = "Moedas de R$ 0,50";
+            // 
+            // lblMoeda05
+            // 
+            this.lblMoeda05.AutoSize = true;
+            this.lblMoeda05.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMoeda05.Location = new System.Drawing.Point(621, 315);
+            this.lblMoeda05.Name = "lblMoeda05";
+            this.lblMoeda05.Size = new System.Drawing.Size(119, 15);
+            this.lblMoeda05.TabIndex = 9;
+            this.lblMoeda05.Text = "Moedas de R$ 0,05";
+            // 
+            // lblQtdMoeda01
+            // 
+            this.lblQtdMoeda01.AutoSize = true;
+            this.lblQtdMoeda01.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQtdMoeda01.Location = new System.Drawing.Point(515, 356);
+            this.lblQtdMoeda01.Name = "lblQtdMoeda01";
+            this.lblQtdMoeda01.Size = new System.Drawing.Size(14, 15);
+            this.lblQtdMoeda01.TabIndex = 10;
+            this.lblQtdMoeda01.Text = "0";
+            // 
+            // lblQtdMoeda05
+            // 
+            this.lblQtdMoeda05.AutoSize = true;
+            this.lblQtdMoeda05.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQtdMoeda05.Location = new System.Drawing.Point(515, 315);
+            this.lblQtdMoeda05.Name = "lblQtdMoeda05";
+            this.lblQtdMoeda05.Size = new System.Drawing.Size(14, 15);
+            this.lblQtdMoeda05.TabIndex = 11;
+            this.lblQtdMoeda05.Text = "0";
+            // 
+            // lblQtdMoeda100
+            // 
+            this.lblQtdMoeda100.AutoSize = true;
+            this.lblQtdMoeda100.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQtdMoeda100.Location = new System.Drawing.Point(515, 149);
+            this.lblQtdMoeda100.Name = "lblQtdMoeda100";
+            this.lblQtdMoeda100.Size = new System.Drawing.Size(14, 15);
+            this.lblQtdMoeda100.TabIndex = 12;
+            this.lblQtdMoeda100.Text = "0";
+            // 
+            // lblQtdMoeda050
+            // 
+            this.lblQtdMoeda050.AutoSize = true;
+            this.lblQtdMoeda050.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQtdMoeda050.Location = new System.Drawing.Point(515, 190);
+            this.lblQtdMoeda050.Name = "lblQtdMoeda050";
+            this.lblQtdMoeda050.Size = new System.Drawing.Size(14, 15);
+            this.lblQtdMoeda050.TabIndex = 13;
+            this.lblQtdMoeda050.Text = "0";
+            // 
+            // lblQtdMoeda010
+            // 
+            this.lblQtdMoeda010.AutoSize = true;
+            this.lblQtdMoeda010.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQtdMoeda010.Location = new System.Drawing.Point(515, 275);
+            this.lblQtdMoeda010.Name = "lblQtdMoeda010";
+            this.lblQtdMoeda010.Size = new System.Drawing.Size(14, 15);
+            this.lblQtdMoeda010.TabIndex = 14;
+            this.lblQtdMoeda010.Text = "0";
+            // 
+            // lblQtdMoeda025
+            // 
+            this.lblQtdMoeda025.AutoSize = true;
+            this.lblQtdMoeda025.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQtdMoeda025.Location = new System.Drawing.Point(515, 234);
+            this.lblQtdMoeda025.Name = "lblQtdMoeda025";
+            this.lblQtdMoeda025.Size = new System.Drawing.Size(14, 15);
+            this.lblQtdMoeda025.TabIndex = 15;
+            this.lblQtdMoeda025.Text = "0";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Swis721 Hv BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label17.Location = new System.Drawing.Point(470, 55);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(300, 32);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "Moedas para o Troco";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.Window;
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.Location = new System.Drawing.Point(70, 207);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(255, 151);
+            this.btnCalcular.TabIndex = 17;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // tbxValorCompra
+            // 
+            this.tbxValorCompra.Location = new System.Drawing.Point(171, 52);
+            this.tbxValorCompra.Name = "tbxValorCompra";
+            this.tbxValorCompra.Size = new System.Drawing.Size(100, 23);
+            this.tbxValorCompra.TabIndex = 18;
+            // 
+            // tbxValorDinheiro
+            // 
+            this.tbxValorDinheiro.Location = new System.Drawing.Point(171, 89);
+            this.tbxValorDinheiro.Name = "tbxValorDinheiro";
+            this.tbxValorDinheiro.Size = new System.Drawing.Size(100, 23);
+            this.tbxValorDinheiro.TabIndex = 19;
+            // 
+            // telaprincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(811, 465);
+            this.Controls.Add(this.tbxValorDinheiro);
+            this.Controls.Add(this.tbxValorCompra);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.lblQtdMoeda025);
+            this.Controls.Add(this.lblQtdMoeda010);
+            this.Controls.Add(this.lblQtdMoeda050);
+            this.Controls.Add(this.lblQtdMoeda100);
+            this.Controls.Add(this.lblQtdMoeda05);
+            this.Controls.Add(this.lblQtdMoeda01);
+            this.Controls.Add(this.lblMoeda05);
+            this.Controls.Add(this.lblMoeda050);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblMoeda01);
+            this.Controls.Add(this.lblMoeda010);
+            this.Controls.Add(this.lblMoeda100);
+            this.Controls.Add(this.lblMoeda025);
+            this.Controls.Add(this.lblValorTroco);
+            this.Controls.Add(this.lblValorDinheiro);
+            this.Controls.Add(this.lblValorCompra);
+            this.Name = "telaprincipal";
+            this.ShowIcon = false;
+            this.Text = "Calcular o Troco em Moedas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValorCompra;
+        private System.Windows.Forms.Label lblValorDinheiro;
+        private System.Windows.Forms.Label lblValorTroco;
+        private System.Windows.Forms.Label lblMoeda025;
+        private System.Windows.Forms.Label lblMoeda100;
+        private System.Windows.Forms.Label lblMoeda010;
+        private System.Windows.Forms.Label lblMoeda01;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblMoeda050;
+        private System.Windows.Forms.Label lblMoeda05;
+        private System.Windows.Forms.Label lblQtdMoeda01;
+        private System.Windows.Forms.Label lblQtdMoeda05;
+        private System.Windows.Forms.Label lblQtdMoeda100;
+        private System.Windows.Forms.Label lblQtdMoeda050;
+        private System.Windows.Forms.Label lblQtdMoeda010;
+        private System.Windows.Forms.Label lblQtdMoeda025;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox tbxValorCompra;
+        private System.Windows.Forms.TextBox tbxValorDinheiro;
+    }
+}
---
+++ CalculoDeTrocoMoeda/View/telaprincipal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoDeTrocoMoeda.View
+{
+    public partial class telaprincipal : Form
+    {
+        public telaprincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Declaramos as variaveis para o tipo decimal
+            if (tbxValorCompra.Text == "")
+            {
+                MessageBox.Show("Nenhum valor encontrado");
+
+            }
+            else
+            {
+
+
+                decimal valorCompra;
+                decimal valorPago;
+                decimal resto;
+
+                //Receber os valores nas variaveis declaradas e converter para decimal
+
+                valorCompra = Convert.ToDecimal(tbxValorCompra.Text);
+                valorPago = Convert.ToDecimal(tbxValorDinheiro.Text);
+
+
+                resto = valorPago - valorCompra;
+
+                lblResultado.Text = resto.ToString();
+
+                // resto = resto % 1;
+
+                int moedas100 = (int)(resto / 1);
+                resto = resto % 1;
+                lblQtdMoeda100.Text = moedas100.ToString();
+
+                int moedas050 = (int)(resto / 0.50m);
+                resto = resto % 0.50m;
+                lblQtdMoeda050.Text = moedas050.ToString();
+
+                int moedas025 = (int)(resto / 0.25m);
+                resto = resto % 0.25m;
+                lblQtdMoeda025.Text = moedas025.ToString();
+
+                int moedas010 = (int)(resto / 0.10m);
+                resto = resto % 0.10m;
+                lblQtdMoeda010.Text = moedas010.ToString();
+
+                int moedas05 = (int)(resto / 0.05m);
+                resto = resto % 0.05m;
+                lblQtdMoeda05.Text = moedas05.ToString();
+
+                int moedas01 = (int)(resto / 0.01m);
+                resto = resto % 0.01m;
+                lblQtdMoeda01.Text = moedas01.ToString();
+            }
+        }
+    }
+}
